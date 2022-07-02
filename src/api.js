8fba49a9a38d2983d4f4b563ccab0ae98da7feca
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-01 21:19:36 -0300

    buid: Retirada de arquivos não usados

--- src/api.js
+++ src/api.js
@@ -1,14 +1,9 @@
 const express = require('express');
 require('express-async-errors');
 
-// ...
 
 const app = express();
 
 app.use(express.json());
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const app = require('./api');
 const errorMiddleware = require('./middleware/error');
 const tokenValidate = require('./middleware/tokenValidate');
 
-// não remova a variável `API_PORT` ou o `listen`
+
 const port = process.env.API_PORT || 3000;
 
-// não remova esse endpoint
+
 app.get('/', (_request, response) => {
   response.send();
 });
